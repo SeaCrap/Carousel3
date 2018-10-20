8d5b4153861d57fd061bcb7533646f3b862de9e2
SeaCrap <506076395@qq.com>
2018-10-20 22:52:13 +0800

    current

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 let $allButtons = $('#buttons > button')
 let $images = $('#images')
+
+let current = 0 
 $allButtons.eq(0).on('click',()=>{
+  console.log(current) //当前位置
   $images.css({transform:'translateX(0px)'})
+  current = 0 // 去了哪里
 })
 $allButtons.eq(1).on('click',()=>{
+  console.log(current)
   $images.css({transform:'translateX(-400px)'})
+  current = 1
 })
 $allButtons.eq(2).on('click',()=>{
+  console.log(current)
   $images.css({transform:'translateX(-800px)'})
+  current = 2
 })
