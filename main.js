60aebf69fcb426334f843dab6b8b9cffdbbc5c5d
SeaCrap <506076395@qq.com>
2018-10-20 23:13:35 +0800

    Copy…clone(true)/append()/prepend()

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 let $allButtons = $('#buttons > button')
 let $images = $('#images')
+let $allImages = $images.children('img')
+//clone(true) 意思是连带子孙元素一起克隆
+$firstCopy = $images.eq(0).clone(true)
+$lastCopy = $images.eq($images.length - 1).clone(true)
+//append()：最后一个元素后追加
+//prepend()：第一个元素前插入
+$images.append($firstCopy)
+$images.prepend($lastCopy)
 
-let current = 0 
+let current = 0 //记录当前点 
 $allButtons.eq(0).on('click',()=>{
-  console.log(current) //当前位置
+  if(current === 2){
+    console.log('说明是从最后一张过来的')
+  }
+
   $images.css({transform:'translateX(0px)'})
   current = 0 // 去了哪里
 })
@@ -13,7 +24,9 @@ $allButtons.eq(1).on('click',()=>{
   current = 1
 })
 $allButtons.eq(2).on('click',()=>{
-  console.log(current)
+  if(current === 0){
+    console.log('说明是从第一张过来的')
+  }
   $images.css({transform:'translateX(-800px)'})
   current = 2
 })
