b0d1cae015d80a9d3950111bb7a49d9db8e1e108
SeaCrap <506076395@qq.com>
2018-10-21 23:57:49 +0800

    gotoslides

--- main.js
+++ main.js
@@ -13,15 +13,6 @@ $slides.css({transform:'translateX(-400px)'})
 bindEvents()
 
 
-
-
-
-
-
-
-
-
-
 // 克隆图片
 function makeFakeSlides(){
   //clone(true) 意思是连带子孙元素一起克隆
@@ -39,7 +30,12 @@ function bindEvents(){
   $('#buttons').on('click','button',(e)=>{
     let $button = $(e.currentTarget) //当前被点击的按钮
     let index = $button.index()//被点击的是第几个
-    //最后一张到第一张
+    goToSlides(index)
+  })
+}
+
+function goToSlides(index){
+   //最后一张到第一张
     if(current === $allButtons.length - 1 && index === 0){
       $slides.css({transform:`translateX(${-($allButtons.length + 1) * 400}px)`})
       .one('transitionend',()=>{
@@ -58,45 +54,4 @@ function bindEvents(){
         $slides.css({transform:`translateX(${-(index+1) * 400}px)`})
       }
       current = index
-    })
-
-  
-//  $allButtons.eq(0).on('click',()=>{
-//    if(current === 2){
-//      $slides.css({transform:'translateX(-1600px)'})
-//        .one('transitionend',()=>{
-//          $slides.hide()
-//            .offset()//计算css会阻断hide和show合并 这样-1600～~400 时就就先隐藏在出现了
-//                  //一般hide之后show没反应 中间就添加offset就可以 offset返回是是一个对象
-//          $slides.css({transform:'translateX(-400px)'})
-//            .show()
-//        })
-//
-//    }else{
-//      $slides.css({transform:'translateX(-400px)'})
-//    } 
-//    current = 0
-//  })
-//  $allButtons.eq(1).on('click',()=>{
-//    console.log(current)
-//    $slides.css({transform:'translateX(-800px)'})
-//    current = 1
-//  })
-//  $allButtons.eq(2).on('click',()=>{
-//    if(current === 0){
-//      $slides.css({transform:'translateX(0px)'})
-//        .one('transitionend',()=>{
-//          $slides.hide()
-//            .offset()
-//          $slides.css({transform:'translateX(-1200px)'})
-//            .show()
-//        })
-//    }else{
-//      $slides.css({transform:'translateX(-1200px)'})
-//    }
-//    current = 2
-//  })
-//
 }
-
-
